0ed5636249cc9e380794f0f605dfddc1e2e86dac
Gbeila Aliu Wahab <aliuwahab@gmail.com>
2019-11-22 14:00:59 +0000

    Add test for api response

--- tests/Feature/StatisticsControllerTest.php
+++ tests/Feature/StatisticsControllerTest.php
@@ -2,16 +2,68 @@
 namespace Test\Feature;
 
 
+use GuzzleHttp\Client;
 use Test\TemperTest;
 
 class StatisticsControllerTest extends TemperTest
 {
 
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->http = new Client(['base_uri' => 'http://localhost:8885/']);
+    }
+
+
+
+
+
     /**
      * @test
      */
-    public function sampleTest(){
-        self::assertTrue(true);
+    public function testOnboardingApiRouteIsValid(){
+
+        $response = $this->http->request('GET', 'user-agent');
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
+
+    /**
+     * @test
+     */
+    public function testOnboardingApiReturnsValidJsonDataType(){
+
+        $response = $this->http->request('GET', '/stats/onboarding');
+        $this->assertEquals(200, $response->getStatusCode());
+        $contentType = $response->getHeaders()["Content-type"];
+        $this->assertEquals("application/json", $contentType[0]);
+    }
+
+
+    /**
+     * @test
+     */
+    public function testOnboardingApiReturnsValidData(){
+
+        $response = $this->http->request('GET', '/stats/onboarding');
+        $this->assertEquals(200, $response->getStatusCode());
+        $contentType = $response->getHeaders()["Content-type"];
+        $this->assertEquals("application/json", $contentType[0]);
+        $data = json_decode($response->getBody()->getContents());
+        $this->assertIsObject($data);
+        $this->assertObjectHasAttribute('data',$data);
+        $this->assertObjectHasAttribute('series',$data->data);
+        $this->assertObjectHasAttribute('categories',$data->data);
+    }
+
+
+
+    public function tearDown() {
+
+        parent::tearDown();
+
+        $this->http = null;
+    }
 }
--- tests/Unit/OnBoardingPresenterTest.php
+++ tests/Unit/OnBoardingPresenterTest.php
@@ -62,4 +62,5 @@ class OnBoardingPresenterTest extends TemperTest
         $this->assertIsArray($data);
     }
 
+
 }
